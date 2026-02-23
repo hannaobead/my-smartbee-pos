487ef9637c07e961094cf1e55d4eb98847574827
hannaobead <hanna.obead@gmail.com>
2026-02-23 12:19:13 +0200

    Update create.js

--- api/create.js
+++ api/create.js
@@ -33,7 +33,7 @@ export default async function handler(req, res) {
 
         const authHeader = `Basic ${Buffer.from(`${clientId}:${password}`).toString('base64')}`;
 
-        const response = await fetch('https://test.smartbee.co.il/api/v1/Documents/create', {
+        const response = await fetch('https://online.smartbee.co.il/api/v1/Documents/create', {
             method: 'POST',
             headers: { 
                 'Content-Type': 'application/json',
